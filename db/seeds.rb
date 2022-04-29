278b8f6d3ee18dc1f4da2e10d0f6cc0936dfc3ec
Taylor Collins <taylorcc33@gmail.com>
2022-04-28 21:24:10 -0600

    seed users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  name = Faker::Name.name
+  email = "#{name.downcase.delete(" ")}@test.com"
+  User.create(name: name, email: email)
+end
